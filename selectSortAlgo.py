10f370b4233e1d1ce6eb76b3538be45cc2ee8c18
munirah alduraibi <80950031+munira-alduraibi@users.noreply.github.com>
2022-12-17 19:20:50 +0300

    Update selectSortAlgo.py

--- selectSortAlgo.py
+++ selectSortAlgo.py
@@ -1,1 +1,23 @@
-# -*- coding: utf-8 -*-"""Created on Fri Mar 18 13:02:25 2022@author: munirah"""def selectionSort( itemsList ):    n = len( itemsList )    for i in range( n - 1 ):         minValueIndex = i        for j in range( i + 1, n ):            if itemsList[j] < itemsList[minValueIndex] :                minValueIndex = j        if minValueIndex != i :            temp = itemsList[i]            itemsList[i] = itemsList[minValueIndex]            itemsList[minValueIndex] = temp    return itemsList 
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 18 13:02:25 2022
+
+@author: munirah
+"""
+# src : https://www.guru99.com/selection-sort-algorithm.html
+def selectionSort( itemsList ):
+    n = len( itemsList )
+    for i in range( n - 1 ): 
+        minValueIndex = i
+
+        for j in range( i + 1, n ):
+            if itemsList[j] < itemsList[minValueIndex] :
+                minValueIndex = j
+
+        if minValueIndex != i :
+            temp = itemsList[i]
+            itemsList[i] = itemsList[minValueIndex]
+            itemsList[minValueIndex] = temp
+
+    return itemsList
+ 
